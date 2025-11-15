c4a38da5a06a8d1daf746440b21f41d2bc48215c
AFMac <amackenzieus@yahoo.com>
2025-11-15 07:37:36 +0000

    updated to address indentation error

--- predict.py
+++ predict.py
@@ -27,8 +27,8 @@ def predict():
     }
     return jsonify(result)
 
-if __name__ == '__main__':
-   #app.run(debug=True, host='0.0.0.0', port=9696) # run the code in local machine with the debugging mode true and port 9696
+#if __name__ == '__main__':
+#   app.run(debug=True, host='0.0.0.0', port=9696) # run the code in local machine with the debugging mode true and port 9696
 
 
 
